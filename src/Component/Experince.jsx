09cbf91b97f6eca1df48a22d4a1d9031827be690
Uddeshya <156643093+Uddeshya-Rawat@users.noreply.github.com>
2025-09-15 10:14:20 +0000

    redifined the expericne section

--- src/Component/Experince.jsx
+++ src/Component/Experince.jsx
@@ -1,17 +1,16 @@
-export default function Experince (){
+export default function Experince() {
     return (
         <div className="text-white sm:p-24 ml-6 p-4 space-y-2 mt-4 ">
             <h2 className="text-4xl font-bold mb-2 underline underline-offset-2 poetsen-one-regular ">Experience</h2>
-            <h3> Intern , The Good Game Theory</h3>
-            <p className='text-xl mt-7 leading-relaxed text-white'>
-                I have a total of 3 months experience in Front End development at Company The Good Game Theory 
-                where i was working as Intern.
-                Created multiple react Components for reusability.
-                Created Student Verification form and worked with RestFull Api's and implemented the API's.
-                Made Different Client side and sever side components for SEO optimizatons .
-                
-
+            <h2 className="text-2xl font-bold mb-2 underline underline-offset-2 poetsen-one-regular "> Intern , The Good Game Theory</h2>
+            <p className="text-xl mt-7 leading-relaxed text-white">
+                I have 3 months of experience as a Front-End Development Intern at
+                <span className="font-semibold">The Good Game Theory</span>. During this time,
+                I built reusable React components, developed a Student Verification Form, and
+                integrated multiple RESTful APIs. I also worked on both client-side and
+                server-side components, focusing on SEO optimization and performance improvements.
             </p>
+
         </div>
     )
 }
