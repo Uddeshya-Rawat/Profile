0bce7a1bac7a0ab452b60d12b9b11cd92fa2a157
Uddeshya <156643093+Uddeshya-Rawat@users.noreply.github.com>
2025-09-15 10:21:23 +0000

    Added Car rental website project

--- src/projectdata.js
+++ src/projectdata.js
@@ -1,27 +1,38 @@
 import task from "./assets/task.png"
 import food from "./assets/food.png"
 import todo from "./assets/Welcomescreen.png"
+import rental from "./assets/rental.png"
 
 
 
 
-const data =[
-     {    title:"Food App",
-        github:'https://github.com/Uddeshya-Rawat/FoodApp',
-        live:'https://peppy-palmier-14ced3.netlify.app/',
-        img:`${food}`
+const data = [
+    {
+        title: "Food ordering website",
+        github: 'https://github.com/Uddeshya-Rawat/FoodApp',
+        live: 'https://peppy-palmier-14ced3.netlify.app/',
+        img: `${food}`
     },
-    {    title:"Dashboard's App",
-        github:"https://github.com/Uddeshya-Rawat/task-management-app.git",
-        live:"https://singular-kelpie-bdd291.netlify.app/",
-        img:`${task}`
+    {
+        title: "Dashboard's website",
+        github: "https://github.com/Uddeshya-Rawat/task-management-app.git",
+        live: "https://singular-kelpie-bdd291.netlify.app/",
+        img: `${task}`
     },
-     {    title:"Full Stack TODO App",
-        github:"https://github.com/Uddeshya-Rawat/FullStack-Task-APP.git",
-        live:"https://thriving-biscotti-c802ee.netlify.app/",
-        img:`${todo}`
+    {
+        title: "Full Stack TODO App",
+        github: "https://github.com/Uddeshya-Rawat/FullStack-Task-APP.git",
+        live: "https://thriving-biscotti-c802ee.netlify.app/",
+        img: `${todo}`
+    },
+    {
+        title: "Car Rental Website",
+        github: "https://github.com/Uddeshya-Rawat/Car-rental/tree/master/Client",
+        live: "dreamy-pastelito-c08a2d.netlify.app",
+        img: `${rental}`
+
     }
-   
+
 
 ]
 
